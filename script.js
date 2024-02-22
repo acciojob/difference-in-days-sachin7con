a5347ce24adf9b35afd67eefa1bf1488b84520d0
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-02-22 14:47:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 var dateDiffInDays = function (date1, date2) {
-  //   write your code here
+  //SGN   write your code here
+	var utc1 = Date.UTC(
+		parseInt(dateOne.substring(0,4)),
+		parseInt(dateOne.substring(5,7)-1),
+		parseInt(dateOne.substring(8))
+	);
+
+	var utc2 = Date.UTC(
+		parseInt(dateTwo.substring(0,4)),
+		parseInt(dateTwo.substring(5,7)-1),
+		parseInt(dateTwo.substring(8))
+	);
+	const miliSecPerDay = 24*60*60*1000;
+	const diffDays = Math.floor(utc2 - utc1)/miliSecPerDay;
+	return diffDays;
 };
 
 // Do not change the code below.
